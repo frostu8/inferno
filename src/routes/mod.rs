7d09afcf07ef1a7d6b09126c40d08c0509f88e99
Dante Helmore <frostu8@protonmail.com>
2025-01-09 12:07:23 -0800

    add redirects from stray links

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -6,7 +6,7 @@ pub mod page;
 use std::fmt::Display;
 
 use axum::{
-    response::ErrorResponse,
+    response::{ErrorResponse, Redirect},
     routing::{get, post},
     Router,
 };
@@ -18,13 +18,34 @@ use crate::ServerState;
 /// Creates a router with all the routes.
 pub fn all() -> Router<ServerState> {
     Router::new()
-        .route(
-            "/~account/login",
-            get(account::login::show).post(account::login::post),
-        )
-        .route("/~account/logout", post(account::logout::post))
+        .nest("/~account", account())
         .route("/~/{*path}", get(page::show).post(page::post))
         .route("/~edit/{*path}", get(page::edit))
+        .route("/", get(redirect_stray))
+        .route("/~", get(redirect_stray))
+        .route("/~/", get(redirect_stray))
+}
+
+/// Creates a router for account routes.
+///
+/// Routes that allow the user to authenticate with password.
+pub fn account() -> Router<ServerState> {
+    Router::new()
+        .route(
+            "/login",
+            get(account::login::show).post(account::login::post),
+        )
+        .route("/logout", post(account::logout::post))
+}
+
+/// Redirects any stray requests to the index page
+///
+/// A stray request is a request that requests:
+/// * The index page @ `/`
+/// * The base url @ `/~`
+#[cfg_attr(debug_assertions, axum::debug_handler)]
+pub async fn redirect_stray() -> Redirect {
+    Redirect::to("/~/Index")
 }
 
 /// Logs an internal server error to the console without showing it to the
